a8de212c46eb952555bdcd4772910e179abc951a
tianwen <tianwen@fotoable.com>
2019-05-13 13:59:32 +0800

    find

--- util.sh
+++ util.sh
@@ -11,3 +11,6 @@ cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1
 
 #获取本机ip
 ifconfig | grep "inet addr:" | awk '{ print $2 }'| sed 's/addr://g'
+
+#搜索文件中的特定字符串内容
+find . -name "*.log" | xargs grep "Error"
